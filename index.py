ad1758fc73421b9f46cd9185800e5f804c16ab4e
Bright Kusi <brightokusi@gmail.com>
2023-10-05 12:57:33 +0000

    Update index.py

--- index.py
+++ index.py
@@ -20,7 +20,6 @@ db_database = config['DB_CONN']['database']
 
 
 
-
 # Step 1. Create the s3 bucket (data lake)
 
 client = boto3.client(
@@ -55,3 +54,68 @@ for table in db_tables:
             'secret': secret_key 
         }
         )
+
+
+# Step 3: Create the Raw Schema in DWH
+conn_details = {
+    'host': dwh_host
+    , 'user': dwh_user
+    , 'password': dwh_password
+    , 'database': dwh_database
+}
+conn = connect_to_dwh(conn_details)
+print('conn successful')
+schema = 'raw_data'
+cursor = conn.cursor()
+
+# ---- Create the dev schema
+create_dev_schema_query = f'CREATE SCHEMA {raw_schema};'
+cursor.execute(create_dev_schema_query)
+
+#  ----- Creating the raw tables
+for query in raw_data_tables:
+    print(f'=================== {query[:50]}')
+    cursor.execute(query)
+    conn.commit()
+
+
+# # -- copy from s3 to Redshift
+
+for table in db_tables:
+    query = f'''
+        copy {schema}.{table} 
+        from '{s3_path.format(bucket_name, table)}'
+        iam_role '{role}'
+        delimiter ','
+        ignoreheader 1;
+    '''
+    cursor.execute(query)
+    conn.commit()
+
+
+cursor.close()
+conn.close()
+
+
+# Step 4: Tansform to a star schema
+conn = connect_to_dwh(conn_details)
+cursor = conn.cursor()
+
+# ------ create schema
+create_staging_schema_query = f'''CREATE SCHEMA {staging_schema};'''
+cursor.execute(create_staging_schema_query)
+conn.commit()
+
+# ------ create facts and dimensions
+for query in transformed_tables:
+    print(f'''------------- {query[:50]}''')
+    cursor.execute(query)
+    conn.commit()
+
+for query in transformation_queries:
+    print(f'''------------- {query[:50]}''')
+    cursor.execute(query)
+    conn.commit()
+
+cursor.close()
+conn.close()
